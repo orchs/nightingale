96cefa4446b54f30f71d3ebe0e7e3c62aa56b5f4
wangkun <wangkun@appexnetworks.com>
2023-01-17 10:16:16 +0800

    update orch alert

--- src/webapi/router/ltw/orch_alert.go
+++ src/webapi/router/ltw/orch_alert.go
@@ -3,10 +3,12 @@ package ltw
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/didi/nightingale/v5/src/models"
 	"github.com/didi/nightingale/v5/src/pkg/ltw"
 	"github.com/didi/nightingale/v5/src/storage"
+	"github.com/didi/nightingale/v5/src/webapi/config"
 	"github.com/gin-gonic/gin"
 	"github.com/toolkits/pkg/ginx"
 	"github.com/toolkits/pkg/logger"
@@ -15,81 +17,59 @@ import (
 )
 
 const LastUpdatePrefix = "ORCH_UPDATE_TIME_"
-const ORCHAlertsApi = "/openapi/v2/alerts"
-const ORCHAlertApi = "/openapi/v2/alert/"
 
-var groupMap = map[int64]string{
-	1001: "orch_国内_告警",
-	1002: "orch_微软_告警",
-	1003: "orch_平安办_告警",
-	1004: "orch_香港_告警",
-	1005: "orch_国电投_告警",
-	1006: "orch_中交_告警",
-	1007: "orch_中国电子_告警",
-	1008: "orch_中能建_告警",
-	1009: "orch_中电科_告警",
-	1010: "orch_万达_告警",
-	1011: "orch_中烟_告警",
-	1012: "orch_有研_告警",
-	1013: "orch_有矿_告警",
-	1014: "orch_中交海外组网_告警",
+func initParams(gid int64) (*config.LtwORCHEnvInfo, error) {
+	for _, c := range config.C.Ltw.ORCHEnvInfos {
+		if c.GroupId == gid {
+			return &c, nil
+		}
+	}
+	return nil, errors.New(fmt.Sprintf("目标业务组没有配置，gid：%v", gid))
 }
 
 func SyncOAlert(c *gin.Context) {
 	// 通过categraf周期性调用
-	gt := ginx.QueryStr(c, "gt")
-	domain := ginx.QueryStr(c, "domain")
-	cid := ginx.QueryStr(c, "cid")
-	cs := ginx.QueryStr(c, "cs")
-	gidStr := ginx.QueryStr(c, "gid")
-	env := ginx.QueryStr(c, "env")
 
-	// 1.检查传参
-	if domain == "" || gt == "" || cid == "" || cs == "" || gidStr == "" || env == "" {
-		ginx.Bomb(400, "传参错误！")
-	}
-	gid, err := strconv.ParseInt(gidStr, 10, 64)
+	// 1.init params
+	gidStr := ginx.QueryStr(c, "gid")
+	gid, _ := strconv.ParseInt(gidStr, 10, 64)
+	cf, err := initParams(gid)
 	if err != nil {
-		ginx.Bomb(500, "gid错误！")
+		ginx.Bomb(500, "参数错误！")
 	}
-	if _, ok := groupMap[gid]; !ok {
-		ginx.Bomb(500, "gid不支持！")
-	}
-	gName := groupMap[gid]
 
 	// 2.登录，获取token
-	token, err := oauthToken(domain, gt, cid, cs, env)
+	token, err := oauthToken(cf.Domain, config.C.Ltw.ORCHGrantType, cf.ClientId, cf.ClientSecret, cf.GroupName)
 	if err != nil {
 		ginx.Bomb(500, "登录错误！")
 	}
 
 	// 3.拉取最新时间段内告警数据
-
 	now := time.Now()
 	bd, _ := time.ParseDuration("-8h00m30s")
 	before := now.Add(bd).Format("2006-01-02T03:04:05Z")
 
 	ctx := context.Background()
-	lastUpdateTIme := LastUpdatePrefix + domain
+	lastUpdateTIme := LastUpdatePrefix + gidStr
 	after, err := storage.Redis.Get(ctx, lastUpdateTIme).Result()
 	if err != nil {
 		d, _ := time.ParseDuration("-9h00m30s")
 		after = now.Add(d).Format("2006-01-02T03:04:05Z")
 	}
 
-	logger.Debugf("开始同步%v数据,同步范围 %v ~ %v", env, after, before)
-	alerts, err := getOAlerts(after, before, domain, env, token)
+	logger.Debugf("开始同步%v数据,同步范围 %v ~ %v", cf.GroupName, after, before)
+	alerts, err := getOAlerts(after, before, cf.Domain, token)
 	if err != nil {
-		ginx.Bomb(500, fmt.Sprintf("拉取%v数据出错:%v", env, err))
+		ginx.Bomb(500, fmt.Sprintf("拉取%v数据出错:%v", cf.GroupName, err))
 	}
-	curAlerts, err := getLocalCurAlerts(gid, gName, domain, token)
+	curAlerts, err := getLocalCurAlerts(gid, cf.GroupName, cf.Domain, token)
 	if err != nil {
-		ginx.Bomb(500, fmt.Sprintf("同步活跃告警信息%v出错:%v", env, err))
+		ginx.Bomb(500, fmt.Sprintf("同步活跃告警信息%v出错:%v", cf.GroupName, err))
 	}
 
 	alerts = append(alerts, curAlerts...)
 	// 4.分析、保存告警数据
-	handleAlerts(gid, gName, alerts)
+	handleAlerts(gid, cf.GroupName, alerts)
 
 	storage.Redis.Set(ctx, lastUpdateTIme, before, time.Duration(time.Hour)).Err()
 	ginx.NewRender(c).Data("", nil)
@@ -128,8 +108,8 @@ func handleAlerts(gid int64, gName string, alerts []ORCHAlertContent) {
 	}
 }
 
-func getOAlerts(after, before, domain, env, token string) ([]ORCHAlertContent, error) {
-	api := fmt.Sprintf("https://%v%v", domain, ORCHAlertsApi)
+func getOAlerts(after, before, domain, token string) ([]ORCHAlertContent, error) {
+	api := domain + config.C.Ltw.ORCHAlertsApi
 	res, err := reqOAlerts(api, token, after, before, "0")
 	if err != nil {
 		return nil, err
@@ -178,7 +158,8 @@ func reqOAlerts(api, token, after, before, page string) (*ORCHAlertsResponse, er
 
 	return &data, nil
 }
-func oauthToken(domain, gt, cid, cs, env string) (string, error) {
+
+func oauthToken(domain, gt, cid, cs, gName string) (string, error) {
 	ctx := context.Background()
 	tokenName := "o_token_" + cid
 
@@ -187,18 +168,18 @@ func oauthToken(domain, gt, cid, cs, env string) (string, error) {
 		return token, nil
 	}
 
-	tokenApi := fmt.Sprintf("https://%v/oauth/token", domain)
+	tokenApi := domain + "/oauth/token"
 	readerStr := fmt.Sprintf("grant_type=%v&client_id=%v&client_secret=%v", gt, cid, cs)
 
 	body, err := ltw.HttpPost(tokenApi, readerStr)
 	if err != nil {
-		logger.Errorf("登录%v失败！%v, 错误信息：%v", env, tokenApi, err)
+		logger.Errorf("登录%v失败！%v, 错误信息：%v", gName, tokenApi, err)
 		return "", err
 	}
 
 	var data ORCHAuthResponse
 	if err := json.Unmarshal(body, &data); err != nil {
-		logger.Errorf("获取 %v token失败！%v, 错误信息：%v", env, tokenApi, err)
+		logger.Errorf("获取 %v token失败！%v, 错误信息：%v", gName, tokenApi, err)
 		return "", err
 	}
 
@@ -220,7 +201,7 @@ func getLocalCurAlerts(gid int64, gName, domain, token string) ([]ORCHAlertConte
 	}
 
 	// 2.遍历搜集到的活跃告警，给对应的orch发送请求，携带status字段，只查找已经恢复的告警
-	api := fmt.Sprintf("https://%v%v", domain, ORCHAlertApi)
+	api := domain + config.C.Ltw.ORCHAlertApi
 	var hisAlerts []ORCHAlertContent
 	for _, v := range list {
 		a, err := ltw.HttpGet(
